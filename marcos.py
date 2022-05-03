3feb05894191e61e4372f6827530c6344e57c9df
GOLDTTeam <88865236+GOLDTTeam@users.noreply.github.com>
2022-05-03 10:48:25 +0300

    Add files via upload

---
+++ marcos.py
@@ -0,0 +1,237 @@
+ 
+''' 
+-------------------------------------------------------------------------
+- Cod BY : MaRcOs
+- Github : https://github.com/MaRcOs
+- Telegram: https://t.me/its9x9
+- Telegram: https://t.me/id_instaup
+-------------------------------------------------------------------------
+      
+''' 
+
+import os , sys, time
+from time import sleep
+
+os.system('clear')
+lib = input('''\033[1;97m
+
+- 1 تحميل مكاتب 
+- دخول مباشر
+
+(∆) ختار ونجب : ''')
+
+if lib == '1':
+    os.system('pip install requests')
+    time.sleep(1)
+    os.system('clear')
+    os.system('pip install user_agent')
+    time.sleep(1)
+    os.system('clear')
+    os.system('pip install names')
+    time.sleep(1)
+    os.system('clear')
+    pass
+else:
+    os.system('clear')
+    pass
+
+import os
+import sys
+import time
+import urllib
+import requests
+import user_agent
+import hashlib
+import names
+import random
+import threading
+import re
+import requests
+from time import sleep
+from multiprocessing import *
+from random import choice
+from time import sleep
+from user_agent import generate_user_agent
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	
+	
+A = '\033[1;91m'
+B = '\033[1;90m'
+C = '\033[1;97m'
+E = '\033[1;92m'
+H = '\033[1;93m'
+K = '\033[1;94m'
+L = "\033[1;95m"
+
+wball = "ðŸ”˜"
+bxall = "ðŸ”´"
+x1 = "ðŸ”µ"
+x2 = "âšª"
+tick = "âšª"
+barem ="ðŸ”µ"
+
+try:
+    print (A + x2 + ' Preparing .......\n')
+    time.sleep(2)
+    os.system('rm 200.txt')
+    os.system('rm 800.txt')
+    os.system('rm 100.txt')
+    os.system('touch 200.txt')
+    os.system('touch 100.txt')
+    os.system('touch 800.txt')
+    os.system('clear')
+except:
+    pass
+
+
+def baner():
+	banera = """ 
+   \033[1;92m  __  __       ____       ___  ____    _____           _     
+   \033[1;97m|  \/  | __ _|  _ \ ___ / _ \/ ___|  |_   _|__   ___ | |___ 
+   \033[1;97m   | |\/| |/ _` | |_) / __| | | \___ \    | |/ _ \ / _ \| / __|
+   \033[1;97m  | |  | | (_| |  _ < (__| |_| |___) |   | | (_) | (_) | \__ \
+   \033[1;97m  |_|  |_|\__,_|_| \_\___|\___/|____/    |_|\___/ \___/|_|___/
+   \033[1;92m 
+
+
+\033[1;93m < \033[1;92mمركز الادوات المجانيه\033[1;93m >  \033[1;91m 
+ — — — — — — — — — — 
+ \033[1;91m(\033[1;92m✫\033[1;91m) \033[1;97mchat    : https://t.me/id_instaup
+ \033[1;91m(\033[1;92m✫\033[1;91m) \033[1;97macont     : اي خدمه @RRLLLL\033[1;91m 
+ — — — — — — — — — \n
+"""              
+	for marcos in banera.splitlines():
+		time.sleep(0.05)
+		print(marcos)
+	
+	
+	
+
+''' 
+-------------------------------------------------------------------------
+- Cod BY : MaRcOs
+- Github : https://github.com/MaRcOs
+- Telegram: https://t.me/its9x9
+- Telegram: https://t.me/id_instaup
+-------------------------------------------------------------------------
+      
+'''
+def ____MaRcOs_____(count, total, size):
+    percent = float(count) / float(total) * 100
+    sys.stdout.write('\r' + str(int(count)).rjust(3, '0') + '/' + str(int(total)).rjust(3, '0') + ' [' + barem * int(percent / 10) * size + ' ' * (10 - int(percent / 10)) * size + ']')
+    
+def _____MaRcOs____(xtext):
+    _______MaRcOs______ = open('100.txt', 'a')
+    _______MaRcOs______.write(str(xtext) + '@')
+    _______MaRcOs______.close()
+
+
+def _______MaRcOs_______(xtext):
+    _______MaRcOs______ = open('200.txt', 'a')
+    _______MaRcOs______.write(str(xtext) + '@')
+    _______MaRcOs______.close()
+
+
+def _______MaRcOs________(xtext):
+    _______MaRcOs______ = open('800.txt', 'a')
+    _______MaRcOs______.write(str(xtext) + '@')
+    _______MaRcOs______.close()
+
+
+def ________MaRcOs________(____userid____, ____coins____):
+    ________MaRcOs_________ = open('successful.txt', 'a')
+    ________MaRcOs_________.write(('\n∆ ID : {} | ∆ Coins: {}').format(____userid____, ____coins____))
+    ________MaRcOs_________.close()
+    
+    
+def _____MaRcOs______(____userid____,____token____,____ID____):
+	_______MaRcOs_______('1')
+	_________MaRcOs_________ ='1234567890qwertyuioplkjhgfdsazxcvbnm'
+	__________MaRcOs_________ = '0987654321'
+	__________MaRcOs__________ = str(''.join(random.choice(_________MaRcOs_________)for i in range(16)))
+	___________MaRcOs__________ = str(''.join(random.choice(_________MaRcOs_________)for i in range(40)))
+	___marcos____ = str(''.join(random.choice(__________MaRcOs_________)for i in range(2)))
+	___marcos_____ = str(''.join(random.choice(__________MaRcOs_________)for i in range(2)))
+	____marcos_____ = str(''.join(random.choice(__________MaRcOs_________)for i in range(2)))
+	____marcos______ = str(''.join(random.choice(__________MaRcOs_________)for i in range(2)))
+	_____marcos______ = names.get_first_name()
+	_____marcos_______ = names.get_first_name()
+	______marcos_______ = str(_____marcos______)+str(____marcos_____)+str(_____marcos_______)+str(____marcos______)
+	____tim____ = time.time()
+	____IP____ = '%s.%s.%s.%s'%(str(___marcos____),str(___marcos_____),str(____marcos_____),str(____marcos______))
+	____url____ = 'https://instaup.marsapi.com/get_likes/user/coins'
+	____access____ = hashlib.md5(b'45:8D:2E:5C:3A:B4:02:2D:B5:DD:3B:E0:98:4F:14:90:CB:5F:B5:45' + bytes(____userid____, 'utf-8')).hexdigest()
+	____data____ ='market=Bazaar&user_id='+str(____userid____)+'&iad='+str(___________MaRcOs__________)+'&user_name='+str(______marcos_______)
+	____home____ = 'null/02:00:00:00:00:00/'+str(____tim____)+'/'+str(__________MaRcOs__________)+'/null/'+str(__________MaRcOs__________)
+	____headers____ = {'market': 'Bazaar','access':str(____access____),'lng': 'en','api_key': 'ABCXYZ123TEST','Nagent':str(____home____),'cnt': 'unknown','version': '51','pkg': 'f2c6d7a5b030a1542f7eb589d5013340','aid':str(___________MaRcOs__________),'Content-Type': 'application/x-www-form-urlencoded','Host': 'instaup.marsapi.com','Connection': 'Keep-Alive','Accept-Encoding': 'gzip','User-Agent': 'okhttp/3.12.1','X-Forwarded-For':str(____IP____)}
+	____responsa____ = requests.post(____url____,data=____data____,headers=____headers____,proxies=urllib.request.getproxies(),allow_redirects=False,verify=False)
+	if ('{"coins":') in str(____responsa____.text):
+		____coinx____ = str(____responsa____.text.split('coins":"')[1])
+		____coin____ = str(____coinx____.split('"')[0])
+		____coins____ = int(____coin____)
+		if int(____coins____) >800 or int(____coins____) >400 :
+			____massage____=('â€¢ SUCCESSFUL COINS UP “\n — — — — — — — — — — \n[∆] ID Target  : '+str(____userid____)+'\n[°] Coin  : '+str(____coins____)+'\n — — — — — — — — — — \nTele $  @id_instaup')
+			requests.get('https://api.telegram.org/bot'+str(____token____)+'/sendMessage?chat_id='+str(____ID____)+'&text='+str(____massage____))
+			________MaRcOs________(____userid____, ____coins____)
+			_____MaRcOs____('1')
+			
+		else:
+			_______MaRcOs________('0')
+			
+	else:
+		_______MaRcOs________('0')
+		
+os.system('clear');baner()
+cpcore = str(cpu_count())
+____cnum____ = int(int(cpcore) * 10)
+____file____ = input(A+" ("+E+"∆"+A+")"+C+ " FILE ID : "+E)
+try:
+    ____iddata____ = open(____file____, 'r').readlines()
+except:
+    print (' Unable To Open')
+    sys.exit()
+
+
+
+
+____ic____ = 0
+os.system('clear');baner()
+____token____ = input(A+" ("+E+"∆"+A+")"+H+ " Enter token :"+C)
+____ID____ = input(A+" ("+E+"∆"+A+")"+H+ " Enter ID Tele :"+C)
+os.system('clear');baner()
+____itotal____ = int(len(____iddata____))
+print (A+" ("+E+"∆"+A+")"+E+ (' ID Accounts : {}{}').format(C, ____itotal____))
+print ('\n')
+
+while ____ic____ <= ____itotal____ - 1:
+    ____userid____ = str(int(____iddata____[____ic____]))
+    ____perx____ = str(int(float(____ic____) / float(____itotal____ - 1) * 100)) + str('%')
+    ____ic____ = ____ic____ + 1
+    ____scx____ = str(int(len(open('100.txt', 'r').read().split('@'))) - 1)
+    ____fcx____ = str(int(len(open('800.txt', 'r').read().split('@'))) - 1)
+    ____pcx____ = str(int(len(open('200.txt', 'r').read().split('@'))))
+    ____pqt____ = int(____pcx____) - int(____fcx____) + int(____scx____)
+    ____cperq____ = str(int(float(float(____pqt____) / float(____cnum____)) * 100)) + str('%')
+    sys.stdout.write(('\r {}MaRcOs{} : {} ({}/{}) [{}]  : [{}] {}({}){} ({}) ').format(E,A, C, ____ic____, ____itotal____, ____perx____, ____cperq____, E, ____scx____, A, ____fcx____))
+    sys.stdout.flush()
+    Process(target=_____MaRcOs______, args=(____userid____,____token____,____ID____)).start()
+    ____ntim____ = int(float(int(____fcx____)) / float(____cnum____))
+    time.sleep(0.2)
+    if int(____pqt____) > ____cnum____:
+        time.sleep(25)
+
+print ('\n')
+while True:
+    ____scx____ = str(int(len(open('100.txt', 'r').read().split('@'))) - 1)
+    ____fcx____ = str(int(len(open('800.txt', 'r').read().split('@'))) - 1)
+    ____icot____ = int(____fcx____) + int(____scx____)
+    ____perv____ = str(int(float(____icot____) / float(____itotal____) * 100)) + str('%')
+    sys.stdout.write(('\r {}({}{}{}) [ Failed: {}{}{} ] & [ Found : {}{} {}]  ').format(E, C, ____perv____, E, C, ____fcx____, E, C, ____scx____, E))
+    sys.stdout.flush()
+    if int(int(____scx____) + int(____fcx____)) == ____itotal____:
+        ____MaRcOs____ = open('successful.txt', 'r').read()
+        print( '\n \n' + x1 + E + ' [ successful coins up ] ' + x1 + E + (' \n {}').format(str(____MaRcOs____)))
+        print ('\n' + x2 + ' Compleated ' + x2)
+        sys.exit()
